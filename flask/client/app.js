f878f209ebebc94c7de1edd58e3a47d11256f35c
Itamar Levi <itamarlevi@Itamars-MacBook-Pro-2.local>
2024-06-01 13:54:03 -0700

    update

---
+++ flask/client/app.js
@@ -0,0 +1,76 @@
+function getBathElement() {
+    var Bath_element = document.getElementsByName("Bath_element");
+    for(var i in Bath_element) {
+        if(Bath_element[i].checked) {
+            return parseInt(i) + 1;
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function getBedroomElement() {
+    var Bedroom_element = document.getElementsByName("Bedroom_element");
+    for(var i in Bedroom_element) {
+        if(Bedroom_element[i].checked) {
+            return parseInt(i) + 1;
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function result(){
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("Sqft_element");         
+    console.log("Got sqft: " + sqft.value);
+
+    var lot_size = document.getElementById("lot_size_element");
+    console.log("Got lot_size: " + lot_size.value);
+
+    var bedrooms = getBedroomElement();
+    console.log("Got bedrooms: " + bedrooms );
+
+    var bathrooms = getBathElement();
+    console.log("Got bathrooms: " + bathrooms);
+    var city = document.getElementById("cities_element");
+    console.log("Got city: " + city.value);
+    var zip_code = document.getElementById("zip_code_element");
+    console.log("Got zip code: " + zip_code.value);
+    var price = document.getElementById("Estimated_price_element");
+    console.log("Got price: " + price.value);
+
+    
+    var url = "http://127.0.0.1:5000/predict_home_price";
+    $.post(url, {
+        city: city.value,
+        bedroom: bedrooms,
+        bath: bathrooms,
+        acre_lot: lot_size.value,
+        zip_code:zip_code.value,
+        sqft: parseFloat(sqft.value),
+        
+    }, function(data, status) {
+        console.log(data)
+        console.log(data.estimate_price);
+        price.innerHTML = "<h2>" + data.estimate_price.toString() +"</h2>";
+        console.log(status);
+    });
+}
+
+function onLoad() {
+    console.log("document loaded");
+    var url = "http://127.0.0.1:5000/get_cities";
+    $.get(url, function(data, status) {
+        console.log("got response for get_location_names request");
+        if (data) {
+            var cities = data.cities;
+            var cities_element = document.getElementById("cities_element");
+            $('#cities_element').empty();
+            for (var i in cities) {
+                var opt = new Option(cities[i]);
+                $('#cities_element').append(opt);
+            }
+        }
+    });
+}
+
+window.onload = onLoad;
--- flask/server/server.py
+++ flask/server/server.py
@@ -1,14 +1,27 @@
 from flask import Flask, request, jsonify
-import util
+from flask_cors import CORS 
+import json, pickle
+import numpy as np
+
+
+__cities = None
+__data_columns = None
+__model = None
 
 
 app = Flask(__name__)
+CORS(app) 
 
-@app.route('/get_cities_names')
+
+@app.route('/')
+def Home():
+    return "Hello"
+
+@app.route('/get_cities')
 def get_cities_names():
 
     response = jsonify({
-        'citiess': util.get_cities_names()
+        'cities': get_cities_names()
     })
 
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -24,10 +37,52 @@ def predict_home_price():
     sqft = float(request.form['sqft'])
 
     response = jsonify({
-        'estimate_price': util.get_estimated_price(city,bedroom,bath,acre_lot,zip_code,sqft)
+        'estimate_price': get_estimated_price(city,bedroom,bath,acre_lot,zip_code,sqft)
     })
     return response
 
+
+def get_estimated_price(city,bedroom,bath,acre_lot,zip_code,sqft):
+    try: 
+        city_index = __data_columns.index(city.lower())
+    except:
+        city_index = -1
+    
+    print(type(__data_columns))
+    x = np.zeros(len(__data_columns))
+    
+    x[0] = bedroom
+    x[1] = bath
+    x[2] = acre_lot 
+    x[3] = zip_code
+    x[4] = sqft
+    
+    if city_index >= 0:
+        x[city_index] = 1
+    return round(__model.predict([x])[0],2)
+
+def get_cities_names():
+    return __cities
+
+def load_saved_artifacts():
+    print("loading saved artifacts")
+    global __data_columns
+    global __cities
+    
+    with open("./columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __cities = __data_columns[5:]
+        
+    global __model
+    with open('./Bay_Area_House_Price_Prediction_Model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    print("loaded")
+
+
 if __name__ == '__main__':
     print('Server Starting...')
+    load_saved_artifacts()
     app.run()
+    
+
+    
